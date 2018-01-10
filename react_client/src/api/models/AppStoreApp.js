5ea736f6ca01633eaf977c81ce0e5597b85b905e
tony ng <tony.hokan@gmail.com>
2018-01-10 19:59:36 +0800

    need larger image

--- react_client/src/api/models/AppStoreApp.js
+++ react_client/src/api/models/AppStoreApp.js
@@ -4,7 +4,7 @@ export default class AppStoreApp {
     this.name = json['im:name'].label
     this.category = json['category'].attributes.label
     this.summary = json['summary'].label
-    this.image = json['im:image'][0].label
+    this.image = json['im:image'][2].label
     this.author = json['im:artist'].label
   }
 }
