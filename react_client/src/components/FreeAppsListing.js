bd433deeaf35ebab2df737a15a2746cff3651816
tony ng <tony.hokan@gmail.com>
2018-01-11 20:07:52 +0800

    as filter do not scroll the page, the lazyload component will not render, so add forceCheck() which check any in viewport element should be loaded to load

--- react_client/src/components/FreeAppsListing.js
+++ react_client/src/components/FreeAppsListing.js
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { ListGroup, ListGroupItem } from 'reactstrap'
-import LazyLoad from 'react-lazyload'
+import LazyLoad, { forceCheck } from 'react-lazyload'
 
 import SpinLoading from './SpinLoading'
 import FreeAppContainer from '../containers/FreeAppContainer'
 
-const FreeAppsListing = (props) => {
-  const { freeApps } = props
-  return (
-    <ListGroup>
-      {freeApps.map((app, index) => {
-        return (
-          <ListGroupItem key={`free-app-${app.id}`}>
-            {
-              // offset mean component will be loaded when it's top edge is 1px from viewport
-              <LazyLoad height={130} once offset={1} placeholder={<SpinLoading/>}>
-                <FreeAppContainer name={app.name} imageUrl={app.image} category={app.category} isCircleImage={(index + 1) % 2 === 0} rank={index + 1} appId={app.id}/>
-              </LazyLoad>
-            }
-          </ListGroupItem>
-        )
-      })}
-      {freeApps.length === 0 && <span>沒有搜尋結果</span>}
-    </ListGroup>
-  )
+class FreeAppsListing extends Component {
+  componentDidUpdate () {
+    forceCheck()
+  }
+
+  render () {
+    const { freeApps } = this.props
+    return (
+      <ListGroup>
+        {freeApps.map((app, index) => {
+          return (
+            <ListGroupItem key={`free-app-${app.id}`}>
+              {
+                // offset mean component will be loaded when it's top edge is 1px from viewport
+                <LazyLoad height={130} resize={true} offset={1} placeholder={<SpinLoading/>}>
+                  <FreeAppContainer name={app.name} imageUrl={app.image} category={app.category} isCircleImage={(index + 1) % 2 === 0} rank={index + 1} appId={app.id}/>
+                </LazyLoad>
+              }
+            </ListGroupItem>
+          )
+        })}
+        {freeApps.length === 0 && <span>沒有搜尋結果</span>}
+      </ListGroup>
+    )
+  }
 }
 
 FreeAppsListing.propTypes = {
